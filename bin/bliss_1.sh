7cadad0c6121ab70a60389d9503943f3a5a411bb
garner1 <silvano.garnerone@gmail.com>
2018-01-16 11:10:21 +0100

    script simplified and adapted to umi-tools

--- bin/bliss_1.sh
+++ bin/bliss_1.sh
@@ -26,57 +26,61 @@ refgen=$HOME/igv/genomes/$genome.fasta
 ################################################################################
 # LOAD DATA FILES
 
-find $fastqDir -maxdepth 1 -type f -iname "*$experiment*.fastq.gz" | sort > filelist_"$experiment"
+# find $fastqDir -maxdepth 1 -type f -iname "*$experiment*.fastq.gz" | sort > filelist_"$experiment"
 
-numb_of_files=`cat filelist_"$experiment" | wc -l`
-r1=`cat filelist_"$experiment" | head -n1`
-echo "R1 is " $r1
-if [ $numb_of_files == 2 ]; then
-    r2=`cat filelist_"$experiment" | tail -n1`
-    echo "R2 is " $r2
-fi
-rm filelist_"$experiment"
+# numb_of_files=`cat filelist_"$experiment" | wc -l`
+# r1=`cat filelist_"$experiment" | head -n1`
+# echo "R1 is " $r1
+# if [ $numb_of_files == 2 ]; then
+#     r2=`cat filelist_"$experiment" | tail -n1`
+#     echo "R2 is " $r2
+# fi
+# rm filelist_"$experiment"
 ################################################################################
 # "$bin"/module/quality_control.sh $numb_of_files $numbproc $out $r1 $r2 
+numb_of_files=1
+# r1=/home/garner1/Work/dataset/bliss/test/test.fastq.gz
+# umi_tools extract --stdin="$r1" --bc-pattern=NNNNNNNNXXXXXXXX --log=processed.log --stdout "$in"/processed.fastq.gz # Ns represent the random part of the barcode and Xs the fixed part
+# r1="$in"/processed.fastq.gz
 # "$bin"/module/prepare_files.sh  $r1 $in $numb_of_files $r2
 ################################################################################
-"$bin"/module/pattern_filtering.sh $in $outcontrol $out $patfile $cutsite
+# "$bin"/module/pattern_filtering.sh $in $outcontrol $out $patfile $cutsite
 "$bin"/module/prepare_for_mapping.sh $numb_of_files $out $aux $outcontrol $auxcontrol $in $cutsite
-"$bin"/module/mapping.sh $numb_of_files $numbproc $refgen $aux $out $experiment 
-"$bin"/module/mapping_quality.sh $numb_of_files $out $experiment $outcontrol $quality $cutsite
-"$bin"/module/umi_joining.sh $numb_of_files $out $experiment $aux $outcontrol $auxcontrol $quality $cutsite
-cat "$datadir"/"$experiment"/outdata/_q"$quality".bed | cut -f-5 |LC_ALL=C uniq -c | awk '{print $2,$3,$4,$5,$6,$1}' | tr " " "," > "$datadir"/"$experiment"/auxdata/aux
-#####UMI filtering
-cp "$datadir"/"$experiment"/auxdata/aux "$datadir"/"$experiment"/outdata/pre_umi_filtering.csv
+# "$bin"/module/mapping.sh $numb_of_files $numbproc $refgen $aux $out $experiment 
+# "$bin"/module/mapping_quality.sh $numb_of_files $out $experiment $outcontrol $quality $cutsite
+# "$bin"/module/umi_joining.sh $numb_of_files $out $experiment $aux $outcontrol $auxcontrol $quality $cutsite
+# cat "$datadir"/"$experiment"/outdata/_q"$quality".bed | cut -f-5 |LC_ALL=C uniq -c | awk '{print $2,$3,$4,$5,$6,$1}' | tr " " "," > "$datadir"/"$experiment"/auxdata/aux
+# #####UMI filtering
+# cp "$datadir"/"$experiment"/auxdata/aux "$datadir"/"$experiment"/outdata/pre_umi_filtering.csv
 
-"$bin"/module/umi_filter_1.sh "$datadir"/"$experiment"/outdata/pre_umi_filtering.csv "$datadir"/"$experiment"/outdata/q"$quality"_aux
+# "$bin"/module/umi_filter_1.sh "$datadir"/"$experiment"/outdata/pre_umi_filtering.csv "$datadir"/"$experiment"/outdata/q"$quality"_aux
 
-"$bin"/module/umi_filter_2.sh "$datadir"/"$experiment"/outdata/q"$quality"_aux "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr
+# "$bin"/module/umi_filter_2.sh "$datadir"/"$experiment"/outdata/q"$quality"_aux "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr
 
-if [ $genome == "hg19" ]; then
-    sed -i.bak 's/chr23/chrX/' "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr
-    sed -i.bak 's/chr24/chrY/' "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr
-fi
+# if [ $genome == "hg19" ]; then
+#     sed -i.bak 's/chr23/chrX/' "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr
+#     sed -i.bak 's/chr24/chrY/' "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr
+# fi
 
-"$bin"/module/umi_filter_3.sh "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr  "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-countDifferentUMI.bed
-sed -i.bak 's/chr23/chrX/' "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-countDifferentUMI.bed
-sed -i.bak 's/chr24/chrY/' "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-countDifferentUMI.bed
+# "$bin"/module/umi_filter_3.sh "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr  "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-countDifferentUMI.bed
+# sed -i.bak 's/chr23/chrX/' "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-countDifferentUMI.bed
+# sed -i.bak 's/chr24/chrY/' "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-countDifferentUMI.bed
 
-# echo "Number of fragments:" > "$datadir"/"$experiment"/outdata/summary.txt
-# wc -l "$datadir"/"$experiment"/indata/r1oneline.fa >> "$datadir"/"$experiment"/outdata/summary.txt
-# echo "Number of fragment with prefix:" >> "$datadir"/"$experiment"/outdata/summary.txt
-# cat "$datadir"/"$experiment"/outdata/*filtered* | paste - - | wc -l >> "$datadir"/"$experiment"/outdata/summary.txt
-echo "Alignment statistics:" >> "$datadir"/"$experiment"/outdata/summary.txt
-samtools flagstat "$datadir"/"$experiment"/outdata/*.sam >> "$datadir"/"$experiment"/outdata/summary.txt
-echo "Number of left and right cuts:" >> "$datadir"/"$experiment"/outdata/summary.txt
-cat "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr | grep -v "_" | cut -f4 | sort | uniq -c >> "$datadir"/"$experiment"/outdata/summary.txt
-echo "Number of DSB locations:" >> "$datadir"/"$experiment"/outdata/summary.txt
-cat "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-countDifferentUMI.bed | grep -v "_" | wc -l >> "$datadir"/"$experiment"/outdata/summary.txt
-echo "Number of UMIs:" >> "$datadir"/"$experiment"/outdata/summary.txt
-cat "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr | grep -v "_" | wc -l >> "$datadir"/"$experiment"/outdata/summary.txt
+# # echo "Number of fragments:" > "$datadir"/"$experiment"/outdata/summary.txt
+# # wc -l "$datadir"/"$experiment"/indata/r1oneline.fa >> "$datadir"/"$experiment"/outdata/summary.txt
+# # echo "Number of fragment with prefix:" >> "$datadir"/"$experiment"/outdata/summary.txt
+# # cat "$datadir"/"$experiment"/outdata/*filtered* | paste - - | wc -l >> "$datadir"/"$experiment"/outdata/summary.txt
+# echo "Alignment statistics:" >> "$datadir"/"$experiment"/outdata/summary.txt
+# samtools flagstat "$datadir"/"$experiment"/outdata/*.sam >> "$datadir"/"$experiment"/outdata/summary.txt
+# echo "Number of left and right cuts:" >> "$datadir"/"$experiment"/outdata/summary.txt
+# cat "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr | grep -v "_" | cut -f4 | sort | uniq -c >> "$datadir"/"$experiment"/outdata/summary.txt
+# echo "Number of DSB locations:" >> "$datadir"/"$experiment"/outdata/summary.txt
+# cat "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-countDifferentUMI.bed | grep -v "_" | wc -l >> "$datadir"/"$experiment"/outdata/summary.txt
+# echo "Number of UMIs:" >> "$datadir"/"$experiment"/outdata/summary.txt
+# cat "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr | grep -v "_" | wc -l >> "$datadir"/"$experiment"/outdata/summary.txt
 
-name=`echo $patfile|rev|cut -d'/' -f1|rev`
-mv "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr "$datadir"/"$experiment"/outdata/"$name"__q"$quality"_chr-loc-strand-umi-pcr.tsv
-mv "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-countDifferentUMI.bed "$datadir"/"$experiment"/outdata/"$name"_chr-loc-countDifferentUMI.bed
-mv "$datadir"/"$experiment"/outdata/summary.txt "$datadir"/"$experiment"/outdata/"$name"__summary.txt
+# name=`echo $patfile|rev|cut -d'/' -f1|rev`
+# mv "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-strand-umi-pcr "$datadir"/"$experiment"/outdata/"$name"__q"$quality"_chr-loc-strand-umi-pcr.tsv
+# mv "$datadir"/"$experiment"/outdata/q"$quality"_chr-loc-countDifferentUMI.bed "$datadir"/"$experiment"/outdata/"$name"_chr-loc-countDifferentUMI.bed
+# mv "$datadir"/"$experiment"/outdata/summary.txt "$datadir"/"$experiment"/outdata/"$name"__summary.txt
 
